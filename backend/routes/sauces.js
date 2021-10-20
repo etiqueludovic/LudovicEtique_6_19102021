417c9c55cf824026fc63fb992554ddf69f0b2af8
etiqueludovic <etique.ludovic@gmail.com>
2021-10-20 15:23:59 +0200

    Create sauces.js

---
+++ backend/routes/sauces.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+const SauceCtrl = require('../controllers/sauces');
+
+router.get('/', auth, multer, SauceCtrl.getAllSauce);
+router.post('/', auth, multer, SauceCtrl.createSauce);
+router.get('/:id', auth, multer, SauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, SauceCtrl.modifySauce);
+router.delete('/:id', auth, SauceCtrl.deleteSauce);
+router.post('/:id/like', auth, SauceCtrl.likeDislike);
+
+module.exports = router;
