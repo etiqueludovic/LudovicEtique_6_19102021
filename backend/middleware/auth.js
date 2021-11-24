34b67671d8e43749ad7987dbe5173b0300fbc3f0
etiqueludovic <etique.ludovic@gmail.com>
2021-11-24 08:04:34 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 
+// construction d'un token pour l'utilisateur
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
