271090df05eb4f399aedd6881680a3fb9dda7c50
etiqueludovic <etique.ludovic@gmail.com>
2021-11-24 08:04:36 +0100

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,11 +1,13 @@
 const multer = require('multer');
 
+// On fige les extensions possible pour les images
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
+// on construit l'upload de fichier
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
