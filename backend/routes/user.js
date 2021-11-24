1ab1fdeb8313ec9cb66ea087023d94b7c49142e3
etiqueludovic <etique.ludovic@gmail.com>
2021-11-24 08:04:49 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-
+// on route les données utilisateurs
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
